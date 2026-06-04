05275f027403ebcd28b1785eade85477f8840861
Arvin <rvin.ramezani@gmail.com>
2026-06-03 22:14:54 -0700

    Change ecosystem.config.js to ecosystem.config.cjs.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
 const cwd = '/var/www/core.unixsee.com';
 const envPath = path.join(cwd, '.env.production');
